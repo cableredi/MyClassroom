31afd5c63ebf7b30fef874d091c7ac0893878bc2
Kimberly Cable <cableredi@comcast.net>
2020-02-20 13:57:44 -0700

    added jwt

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -161,7 +161,7 @@ export default class App extends Component {
         })
         .then(this.setClasses)
         .catch(error => this.setState({ error }))
-      }
+    }
   }
 
 
--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import TokenService from '../../Services/token-service';
+import AuthApiService from '../../Services/auth-api-service';
 import ValidateError from '../ValidateError/ValidateError';
 
 const Required = () => (
@@ -45,7 +46,7 @@ export default class LoginForm extends Component {
   }
 
   /***********************/
-  /* handleSubmitJwtAuth */
+  /* handleSubmitBasicAuth */
   /***********************/
   handleSubmitBasicAuth = e => {
     e.preventDefault();
@@ -64,6 +65,26 @@ export default class LoginForm extends Component {
     this.props.onLoginSuccess();
 
   };
+
+  handleSubmitJwtAuth = ev => {
+    ev.preventDefault()
+    this.setState({ error: null })
+    const { user_name, password } = ev.target
+    
+    AuthApiService.postLogin({
+      user_name: user_name.value,
+      password: password.value,
+    })
+      .then(res => {
+        user_name.value = '';
+        password.value = '';
+        TokenService.saveAuthToken(res.authToken);
+        this.props.onLoginSuccess();
+      })
+      .catch(res => {
+        this.setState({ error: res.error });
+      })
+    }
 
   /************************/
   /* Validate Form Fields */
@@ -106,7 +127,7 @@ export default class LoginForm extends Component {
     return (
       <form
         className='Login__form'
-        onSubmit={this.handleSubmitBasicAuth}
+        onSubmit={this.handleSubmitJwtAuth}
       >
         <div className="required">* Required Fields</div>
 
