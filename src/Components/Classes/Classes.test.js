da48457ad5c9db7d12860f64d30d0910d251271d
Kimberly Cable <cableredi@comcast.net>
2020-02-26 16:49:15 -0700

    added some test cases

--- src/Components/Classes/Classes.test.js
+++ src/Components/Classes/Classes.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import ClassesItems from './ClassItems/ClassItems';
+import ClassesList from './ClassesList/ClassesList';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -10,7 +10,7 @@ it('renders without crashing', () => {
 
   ReactDOM.render(
     <BrowserRouter>
-      <ClassesItems classes={classesProps} />
+      <ClassesList classes={classesProps} />
     </BrowserRouter>,
     div
   );
