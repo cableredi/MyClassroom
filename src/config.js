c1e1bdc14e34798258288995267e0e812af0aaf3
Kimberly Cable <cableredi@comcast.net>
2020-02-26 18:05:55 -0700

    production db chg

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
-  API_ENDPOINT_ASSIGNMENTS: `http://localhost:8000/api/assignments`,
-  API_ENDPOINT_CLASSES: `http://localhost:8000/api/classes`,
-  API_ENDPOINT_USERS: `http://localhost:8000/api/users`,
-  API_ENDPOINT_AUTH: `http://localhost:8000/api/auth`,
+  API_ENDPOINT_ASSIGNMENTS: `https://limitless-woodland-34811.herokuapp.com/api/assignments`,
+  API_ENDPOINT_CLASSES: `https://limitless-woodland-34811.herokuapp.com/api/classes`,
+  API_ENDPOINT_USERS: `https://limitless-woodland-34811.herokuapp.com/api/users`,
+  API_ENDPOINT_AUTH: `https://limitless-woodland-34811.herokuapp.com/api/auth`,
   API_KEY: process.env.REACT_APP_API_KEY,
   TOKEN_KEY: 'myclassroom-client-auth-token',
 }
